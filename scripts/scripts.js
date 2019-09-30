94beeb2cbdee5bc2ef9f5589be6396c63c2236f8
breanna-hansen <breannahansen@gmail.com>
2019-09-30 05:14:10 -0600

    Start json, add to objects page, update nav links

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,10 +4,20 @@
 var navMenu = {
     javascript: "javascript.html",
     object : "javascript-objects.html",
-    json     :  "json.html"
+    json : "json.html"
 };
     // Display some data from the object:
 document.getElementById("objReference").innerHTML =
-"Uses bracket notation to reference<br>" + navMenu["javascript"];
+"Uses bracket notation to reference<br>&quot;" + navMenu["javascript"] + "&quot;";
 document.getElementById("objReferenceDot").innerHTML =
-"Uses dot method to reference<br>" + navMenu.javascript;
+"Uses dot method to reference<br>&quot;" + navMenu.javascript + "&quot;";
+
+//Object Method Example
+var navMenu2 = {
+    name : "Javascript",
+    url : "javascripts.html",
+    createText : function(arg1, arg2, arg3) {
+        return arg1 + this.url + arg2 + this.name + arg3;
+    }
+};
+document.getElementById("objMethod").innerHTML = navMenu2.createText("&lt;a href&equals;&quot;", "&quot;&gt;", "&lt;&sol;a&gt");
