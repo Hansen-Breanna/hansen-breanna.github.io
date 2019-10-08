5f9d2f527de1118597f7aac9036098b53ec27f0a
breanna-hansen <breannahansen@gmail.com>
2019-10-08 08:28:10 -0600

    Add DOM manipulation for paragraph

--- scripts/DOM.js
+++ scripts/DOM.js
@@ -1,3 +1,10 @@
+// Paragraph stating how nav was made
+var statement = document.createElement("p");                 // Create a <li> node
+var textnode = document.createTextNode("This nav was built using DOM Manipulation.");         // Create a text node
+statement.appendChild(textnode);                              // Append the text to <li>
+document.getElementById("introPara").appendChild(statement); 
+console.log(statement);
+
 // Request JSON file to create nav with DOM Manipulation
 var newNav = new XMLHttpRequest();
 var requestURL = 'JSON/link-names.json';
@@ -9,10 +16,4 @@ newNav.send();
 //Functions that parses string and displays output
 newNav.onload = function parseNav() {
     var nav = JSON.parse(newNav.responseText);
-    var navIntro = 
-    var list = document.createElement("ul");                 // Create a <li> node
-    var textnode = document.createTextNode("Water");         // Create a text node
-    list.appendChild(textnode);                              // Append the text to <li>
-    document.getElementById("nav-menu").appendChild(list); 
-    console.log(nav);
 }
