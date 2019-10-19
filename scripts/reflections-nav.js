c84b6e31dae8ae2422fd78c7de9a384a4e0da9b1
breanna-hansen <breannahansen@gmail.com>
2019-10-18 21:46:15 -0600

    add nav and scripts to reflections

---
+++ scripts/reflections-nav.js
@@ -0,0 +1,23 @@
+// NAVIGATION MENU
+
+   //Store XMLH request and JSON file location in variables
+   var newNav = new XMLHttpRequest();
+   var requestURL = '../JSON/link-names-for-reflections.json';
+
+   //HTTP call using the requestURL variable
+   newNav.open("Get", requestURL, true);
+   newNav.send();
+
+   //Functions that parses string and displays output
+   newNav.onload = function parseNav() {
+       var nav = JSON.parse(newNav.responseText);
+       console.log(nav.urlList);
+       var finalString = "";
+       for(var i = 0; i < nav.urlList.length; i++){
+         var navItem = nav.urlList[i];
+         finalString += "<li class='urlList'><a style='color:#fff;' href='" + navItem.url + "'>" + 
+         navItem.name + "</a></li><br>";
+       }
+       document.getElementById('nav-menu').innerHTML = "<ul>" + finalString + "</ul>";
+       console.log(nav);
+   }
