b0920dbfb3e8be7346a30523cd94521fe1c3318e
breanna-hansen <breannahansen@gmail.com>
2019-10-30 22:49:52 -0600

    add third row for larger devices and buttons to row

--- final-project/scripts/accordian.js
+++ final-project/scripts/accordian.js
@@ -6,14 +6,17 @@ function listInCategory(listID, categoryUrl) {
     lists.send();
     lists.onload =  function (details) {
         let listData = JSON.parse(lists.responseText);
+        console.log(listData);
+
         var topBooks = "";
         for (var i = 0; i < 14; i++) {
             var number = i + 1;
-            topBooks += "<div><h2 class='bookTitle'>" + number + ". " + listData.results.books[i].title + "</h2><br>by: " 
-            + listData.results.books[i].author + "<br>"
-            + "<img class='bookImage' src='" + listData.results.books[i].book_image + "' />" 
-            + listData.results.books[i].description 
-            + "<br><div id='buttons'><button class='button-trio' type='button' onclick='addItem()'>Add</button>"
+            topBooks += "<div><h2 class='bookTitle'>" + number + ". " + listData.results.books[i].title + "</h2><p class='noBottomMg'><strong>by:</strong> " 
+            + listData.results.books[i].author + "</p><p class='noTopMg'><strong>Isbn:</strong>"
+            + listData.results.books[i].isbns[0].isbn10 + "</p>"
+            + "<img class='bookImage' src='" + listData.results.books[i].book_image + "' />"
+            + "<p>" + listData.results.books[i].description + "</p>"
+            + "<div id='buttons'><button class='button-trio' type='button' onclick='addItem()'>Add</button>"
             + "<button class='button-trio' type='button' onclick='removeItem()'>Remove</button>"
             + "<a href='" + listData.results.books[i].amazon_product_url + "/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0399590501&linkCode=as2&tag=budgetmealrecipesfb-20&linkId=1cecfc480f866b384c941a1069a078f7"
             + "'><button class='button-trio' type='button' onclick='buyItem()'>Buy</button></a></div></div>";
