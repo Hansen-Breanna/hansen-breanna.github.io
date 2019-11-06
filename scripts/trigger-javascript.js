f53ff65cf0bbe2c9084bb596f952c787610b555d
breanna-hansen <breannahansen@gmail.com>
2019-11-06 12:15:48 -0700

    add tiggered transition, complete animation page

---
+++ scripts/trigger-javascript.js
@@ -0,0 +1,9 @@
+function fadeOut() {
+    var square = document.getElementById("square");
+    square.style.opacity = 0;
+}
+
+function fadeIn() {
+    var showSquare =  document.getElementById("square");
+    showSquare.style.opacity = 1;
+}
