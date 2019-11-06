5b05ce1302adab17a32a6230a0c903f50f33e7ba
breanna-hansen <breannahansen@gmail.com>
2019-11-06 15:20:42 -0700

    add multiple element transition to triggers

---
+++ final-project/scripts/list.js
@@ -0,0 +1,9 @@
+function showList() {
+    var showPopUp = document.getElementById('list');
+    showPopUp.style.display = "block";
+}
+
+function hideList() {
+    var hidePopUp = document.getElementById('list');
+    hidePopUp.style.display = "none";
+}
--- scripts/trigger-javascript.js
+++ scripts/trigger-javascript.js
@@ -7,3 +7,19 @@ function fadeIn() {
     var showSquare =  document.getElementById("square");
     showSquare.style.opacity = 1;
 }
+
+function showList() {
+    var showPopUp = document.getElementById('list');
+    showPopUp.style.display = "block";
+}
+
+function hideList() {
+    var hidePopUp = document.getElementById('list');
+    hidePopUp.style.display = "none";
+}
+
+function rollingBox() {
+    var changeBox = document.getElementById('rolling-box');
+    changeBox.style.transition = "all 2s";
+    changeBox.id = "rollingBox";
+}
