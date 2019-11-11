4a571489e0ab60486900a1ae5f326a6bde28c5eb
breanna-hansen <breannahansen@gmail.com>
2019-11-11 09:35:24 -0700

    add canvas buy and remove buttons

--- final-project/scripts/accordian.js
+++ final-project/scripts/accordian.js
@@ -14,19 +14,18 @@ function listInCategory(listID, categoryUrl) {
 
             //Get book title and set to local storage
             var bookTitle = listData.results.books[i].title;
-            localStorage.setItem("title" + [i], bookTitle);           
 
             //Get URL and set to local storage
-            var bookURL = listData.results.books[i].amazon_product_url +  "/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0399590501&linkCode=as2&tag=budgetmealrecipesfb-20&linkId=1cecfc480f866b384c941a1069a078f7";
-            localStorage.setItem("url" + [i], bookURL);   
+            var bookURL = listData.results.books[i].amazon_product_url + 
+                "/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0399590501&linkCode=as2&tag=budgetmealrecipesfb-20&linkId=1cecfc480f866b384c941a1069a078f7";  
 
             //Loop through and create all book info
-            topBooks += "<div><h2 class='bookTitle'>" + number + ". " + localStorage.getItem("title" + [i]) + "</h2><p class='noBottomMg'><strong>by:</strong> " 
+            topBooks += "<div><h2 class='bookTitle'>" + number + ". " + bookTitle + "</h2><p class='noBottomMg'><strong>by:</strong> " 
             + listData.results.books[i].author + "</p>"
             + "<img class='bookImage' src='" + listData.results.books[i].book_image + "' />"
             + "<p>" + listData.results.books[i].description + "</p>"
             + "<div id='buttons'>"
-            + "<button class='button-trio' type='button' onclick='addItem('" + bookTitle + "', '" + bookURL + "')'>Add</button>"
+            + "<button class='button-trio' type='button' onclick='addItem(" + bookTitle + ", " + bookURL + ")'>Add</button>"
             + "<button class='button-trio' type='button' onclick='removeItem()'>Remove</button>"
             + "<a href='" + bookURL
             + "'><button class='button-trio' type='button' onclick='buyItem()'>Buy</button></a></div></div>";
--- final-project/scripts/list.js
+++ final-project/scripts/list.js
@@ -8,6 +8,8 @@ function showList() {
   }
 
 function addItem(bookTitle, bookURL) {
+  localStorage.setItem("title", bookTitle);  
+  localStorage.setItem("url", bookURL);   
   var addItem = document.getElementById('addToList');
   var listItem = document.createElement('li');
   addItem.appendChild(listItem);
@@ -16,3 +18,20 @@ function addItem(bookTitle, bookURL) {
   buyButton.textContent = "<a href='" + bookURL + "'>Buy</a>";
   console.log(listItem);
 }
+
+//Remove Button
+var c = document.getElementById("removeButtonBox");
+var ctx = c.getContext("2d");
+ctx.moveTo(60,75);
+ctx.lineTo(230,75);
+ctx.lineWidth = 6;
+ctx.stroke();
+
+//Buy Button
+var c = document.getElementById("buyButtonBox");
+var ctx = c.getContext("2d");
+ctx.font = "110px Arial";
+ctx.fillText("$", 120, 120);
+ctx.moveTo(120,120);
+ctx.lineTo(120,220);
+ctx.stroke;
