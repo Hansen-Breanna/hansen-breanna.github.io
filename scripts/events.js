9ee9cd328b914fdb9dd24f72432f2b2744184fcc
breanna-hansen <breannahansen@gmail.com>
2019-11-12 11:27:26 -0700

    add touchstart and click event

--- scripts/events.js
+++ scripts/events.js
@@ -1,15 +1,18 @@
-
-
-function touchstart(ev) {
-    document.getElementsByClassName('grayBg').addEventListener('touchstart', touchStartPractice);
-    console.log( ev.touches );
+//Click
+function clickListener() {
+    document.getElementById('clickListener').addEventListener('click', clickList(event));
+    console.log(event);
 }
 
-function touchStartPractice() {
-
+function clickList(event) {
+    event.target.previousSibling.textContent = "Found using addEventListener('click', function(event)). Also, check the console!"
 }
 
-function touchAnimate() {
-    document.getElementById('touchstart').classList.add('touchstart');
+function touchstart(paraChange) {
+    var paraChange = document.getElementById('touchstart');
+    paraChange.addEventListener('touch', touchChange());
+}
 
+function touchChange(paraChange) {
+    paraChange.target.className = "touchstart";
 }
