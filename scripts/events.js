3ca67634fab6694a0ab793d311fe838486812916
breanna-hansen <breannahansen@gmail.com>
2019-11-12 22:11:21 -0700

    get touchstart to change id's

--- scripts/events.js
+++ scripts/events.js
@@ -12,9 +12,9 @@ function clickList(event) {
 function touchstart() {
     var paraChange = document.getElementById('touchstart');
     paraChange.addEventListener('touch', touchChange(event));
+    console.log(event);
 }
 
 function touchChange(event) {
-    event.target.className = "touchstart";
-    console.log("hello");
+    event.target.id = "touchStartChange";
 }
