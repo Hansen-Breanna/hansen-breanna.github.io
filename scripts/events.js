f32b8c800e4347e6cf47f1934e650017e7d31bb1
breanna-hansen <breannahansen@gmail.com>
2019-11-14 12:23:32 -0700

    attempt touchmove

--- scripts/events.js
+++ scripts/events.js
@@ -28,3 +28,50 @@ function touchend() {
 function touchEndChange(event) {
     event.target.id = "touchEndChange";
 }
+
+//Animation
+window.onload = animateBall();
+
+function animateBall() {
+    var ballContainer = document.getElementById('ball-container');
+    ballContainer.classList += 'ball-container';
+    var ball = document.createElement('div');
+    ball.classList += 'rolling-ball';
+    ballContainer.appendChild(ball);
+}
+
+//Touchmove
+window.onload = function () {
+    //select the thing we wanna drag
+    var butterfly = document.getElementById('touchmove');
+    //listen to the touchmove event, every time it fires, grab the location of the touch
+    //then assign it to mustachio
+    butterfly.addEventListener('touchmove', function (ev) {
+        //grab the location of the touch
+        var touchLocation = ev.targetTouches[0];
+        //assign mustachio new coordinates based on the touch
+        butterfly.style.left = touchLocation.pageX + 'px';
+        butterfly.style.top = touchLocation.pageY + 'px';
+    })
+    butterfly.addEventListener('touchend', function (ev) {
+        //current mustachio position when dropped
+        var x = parseInt(butterfly.style.left);
+        var y = parseInt(butterfly.style.top);
+        //check to see if that position meets our constraints
+        if (x < 388 || x > 646) {
+            butterfly.style.left = '450px';
+            butterfly.style.top = '175px';
+        }
+        if (y < 100 || y > 356) {
+            butterfly.style.left = '450px';
+            butterfly.style.top = '175px';
+        }
+    })
+}
+
+
+
+//Toggle
+function toggleBox() {
+    alert('The toggle event has beenactivated.');
+}
