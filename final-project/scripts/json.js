a70653cfc8c1627bf5edd537f2fa2f1d4eb676de
breanna-hansen <breannahansen@gmail.com>
2019-12-04 22:57:27 -0700

    add mixin in accordian

--- final-project/scripts/json.js
+++ final-project/scripts/json.js
@@ -1,21 +1,21 @@
-// NAVIGATION MENU
+// // NAVIGATION MENU
 
-   //Store XMLH request and JSON file location in variables
-   var newNav = new XMLHttpRequest();
-   var requestURL = 'JSON/link-names.json';
+//    //Store XMLH request and JSON file location in variables
+//    var newNav = new XMLHttpRequest();
+//    var requestURL = 'JSON/link-names.json';
 
-   //HTTP call using the requestURL variable
-   newNav.open("Get", requestURL, true);
-   newNav.send();
+//    //HTTP call using the requestURL variable
+//    newNav.open("Get", requestURL, true);
+//    newNav.send();
 
-   //Functions that parses string and displays output
-   newNav.onload = function parseNav() {
-       var nav = JSON.parse(newNav.responseText);
-       var finalString = "";
-       for(var i = 0; i < nav.urlList.length; i++){
-         var navItem = nav.urlList[i];
-         finalString += "<li class='urlList'><a style='color:#fff;' href='" + navItem.url + "'>" + 
-         navItem.name + "</a></li><br>";
-       }
-       document.getElementById('nav-menu').innerHTML = "<ul>" + finalString + "</ul>";
-   }
+//    //Functions that parses string and displays output
+//    newNav.onload = function parseNav() {
+//        var nav = JSON.parse(newNav.responseText);
+//        var finalString = "";
+//        for(var i = 0; i < nav.urlList.length; i++){
+//          var navItem = nav.urlList[i];
+//          finalString += "<li class='urlList'><a style='color:#fff;' href='" + navItem.url + "'>" + 
+//          navItem.name + "</a></li><br>";
+//        }
+//        document.getElementById('nav-menu').innerHTML = "<ul>" + finalString + "</ul>";
+//    }
